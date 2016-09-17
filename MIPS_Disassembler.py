58e4504714fc24c8b049e3c39e3c920aec45bed4
Andy O'Connell <andrewoconnell89@gmail.com>
2016-09-17 13:09:36 -0400

    Sync 2
    removed bugs

--- MIPS_Disassembler.py
+++ MIPS_Disassembler.py
@@ -1,5 +1,17 @@
 class MIPS_Disassembler(object):
+    TEST_INSTRUCTIONS = [   0x022DA822,
+                            0x8EF30018,
+                            0x12A70004,
+                            0x02689820,
+                            0xAD930018,
+                            0x02697824,
+                            0xAD8FFFF4,
+                            0x018C6020,
+                            0x02A4A825,
+                            0x158FFFF6,
+                            0x8E59FFF0,
+                        ]
 
     def __init__(self):
-        self.op
-        self.
+        
+        
